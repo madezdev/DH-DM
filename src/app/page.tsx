1d394d489ae513a13a037346c61270875e27571b
Martin Hernandez <martinhernandez83@gmail.com>
2024-09-25 22:42:04 -0300

    Refactorizar componentes compartidos y exportaciones
    Se refactorizaron los componentes compartidos y se realizaron cambios en las exportaciones para mejorar la estructura del código.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,65 @@
-import Image from "next/image";
+import Image from 'next/image'
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      {/* <Navbar /> */}
+      <div className='flex flex-col flex-grow'>
+        <section className='relative flex-grow flex flex-col justify-between'>
+          <Image
+            src='/images/landing.png'
+            alt='Digital Money'
+            width={1000}
+            height={1000}
+            className='object-cover object-center absolute -top-10 left-0 z-[-1] w-full h-full md:hidden'
+          />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src='/images/landing-xxl.png'
+            alt='Digital Money'
+            fill
+            className='object-cover object-top absolute top-0 left-0 z-[-1] w-full h-full hidden md:block'
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+
+          <section className='w-full mx-[4vw] flex flex-col justify-center gap-4'>
+            <div className='mt-[44px] xl:mt-[74px]'>
+              <h1 className='text-white text-[27px] md:text-[48px] leading-tight w-[190px] md:w-[418px]'>
+                De ahora en adelante, hacés más con tu dinero.
+              </h1>
+            </div>
+            <div className='bg-primary h-[4px] w-6' />
+            <div className='w-[148px] xl:w-[420px]'>
+              <h2 className='text-primary text-[21.50px] md:text-[34px] '>
+                Tu nueva <strong>billetera virtual</strong>
+              </h2>
+            </div>
+          </section>
+          <div className='bg-primary w-full h-[280px] md:h-[430px] xl:h-[148px] rounded-t-3xl relative'>
+            <div className='px-[4vw] md:px-[14vw] xl:px-0 absolute left-0 -top-16 md:-top-20 xl:-top-32 flex flex-col xl:flex-row xl:justify-center xl:gap-8 w-full'>
+              <div className='bg-white p-2 md:p-8 rounded-xl xl:w-[500px] xl:h-[246px] xl:p-[30px]'>
+                <h3 className='text-[28px] md:text-[40px] border-b-2 border-primary'>
+                  <strong>Transferí dinero</strong>
+                </h3>
+                <p className='text-[16px] md:text-[20px] mt-2'>
+                  Desde Digital Money House vas a poder transferir dinero a
+                  otras cuentas, así como también recibir transferencias y
+                  nuclear tu capital en nuestra billetera virtual.
+                </p>
+              </div>
+              <div className='bg-white p-2 md:p-8 rounded-xl mt-4 xl:mt-0 xl:w-[500px] xl:h-[246px] xl:p-[30px]'>
+                <h3 className='text-[28px] md:text-[40px] border-b-2 border-primary'>
+                  <strong>Pago de servicios</strong>
+                </h3>
+                <p className='text-[16px] md:text-[20px] mt-2'>
+                  Pagá mensualmente los servicios en 3 simples clicks. Fácil,
+                  rápido y conveniente. Olvidate de las facturas en papel.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      {/* <Footer /> */}
+    </>
+  )
 }
