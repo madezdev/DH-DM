fc61012dfa3d6987e03830295cc3260da8395125
Martin Hernandez <madezdev@gmail.com>
2024-12-08 11:15:32 -0300

    Agregar funcionalidad de depósito: implementar páginas de transferencia bancaria y selección de tarjeta; añadir componentes relacionados y actualizar interfaces

---
+++ src/app/(dashboard)/deposit/bank-transfer/page.tsx
@@ -0,0 +1,10 @@
+import { Container } from '@/components'
+import React from 'react'
+export default function BankTransferPage() {
+  
+  return (
+    <Container title='Cargar dinero'>
+      <h1>Hello Page</h1>
+    </Container>
+  )
+}
